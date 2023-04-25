24298189979a6ad3677d9d467f973294df43bff4
Garrett Varenhorst <garrettvarenhorst@gmail.com>
2023-04-25 13:07:33 -0500

    Add attr_reader, initialize, loop? and total_distance methods

--- lib/ride.rb
+++ lib/ride.rb
@@ -1,3 +1,25 @@
 class Ride
+  attr_reader :name,
+              :distance,
+              :loop,
+              :terrain
+  def initialize(info)
+    @name = info[:name]
+    @distance = info[:distance]
+    @loop = info[:loop]
+    @terrain = info[:terrain]
+  end
 
+  def loop?
+    @loop
+  end
+
+  def total_distance
+    base_distance = @distance
+    if loop? == false
+      base_distance + base_distance
+    else
+      base_distance
+    end
+  end
 end
